edf41db6b9b762cf8c722b3390793724cb59ebcc
= <=>
2021-08-22 17:45:01 +0100

    initial commit

---
+++ src/main/java/com/phoebelord/whatsfortea/WhatsforteaApplication.java
@@ -0,0 +1,13 @@
+package com.phoebelord.whatsfortea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatsforteaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatsforteaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/phoebelord/whatsfortea/WhatsforteaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phoebelord.whatsfortea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatsforteaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
